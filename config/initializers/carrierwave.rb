e66b3839a5c3d361733fa50cb826b41f760ed950
profh <profh@cmu.edu>
2011-07-08 11:02:38 -0400

    Changing the s3 bucket for this project

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider => 'AWS'
   }
-  config.fog_directory = "thalia-test" # name_of_bucket required
+  config.fog_directory = "uif-theater" # name_of_bucket required
   config.fog_host       = 'http://s3.amazonaws.com'               # optional, defaults to nil
   config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
