7693e42757978481a3a13f1a5613a4c994fb64b8
profh <profh@cmu.edu>
2011-07-11 17:20:26 -0400

    Fixed search bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,8 +20,8 @@ class HomeController < ApplicationController
   def search
     @query=params[:query]
     @c = Item.search(@query, "costumes")
-    @p = Item.search(@query, "costumes")
-    @s = Item.search(@query, "costumes")
+    @p = Item.search(@query, "props")
+    @s = Item.search(@query, "staging")
     @costumes = Kaminari.paginate_array(@c).page(params[:page]).per(21)
     @props = Kaminari.paginate_array(@p).page(params[:page]).per(21)
     @staging = Kaminari.paginate_array(@s).page(params[:page]).per(21)
