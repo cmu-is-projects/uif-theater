d3b505ac9096583bd728cf8c7f30794bc6d42287
profh <profh@cmu.edu>
2011-07-11 14:40:04 -0400

    Trying to fix pagination issue

--- app/models/item.rb
+++ app/models/item.rb
@@ -46,9 +46,9 @@ class Item < ActiveRecord::Base
 	
 	# Scopes
 	scope :alphabetical, order('items.name')
-  scope :just_props, joins(:categories).where("categories.id IN (#{Category.all_ids_associated_with("Props").join(",")})") #.group(:item_id)
-  scope :just_costumes, joins(:categories).where("categories.id IN (#{Category.all_ids_associated_with("Costumes").join(",")})") #.group(:item_id)
-  scope :just_staging, joins(:categories).where("categories.id IN (#{Category.all_ids_associated_with("Staging").join(",")})") #.group(:item_id)
+  scope :just_props, joins(:categories).where("categories.id IN (#{Category.all_ids_associated_with("Props").join(",")})") 
+  scope :just_costumes, joins(:categories).where("categories.id IN (#{Category.all_ids_associated_with("Costumes").join(",")})") 
+  scope :just_staging, joins(:categories).where("categories.id IN (#{Category.all_ids_associated_with("Staging").join(",")})") 
   
   # scope :just_props, lambda ("subcategory_of IN (#{Category.all_ids_associated_with("Props").join(",")}) OR name = 'Props'")
   # scope :just_costumes, where("subcategory_of IN (#{Category.all_ids_associated_with("Costumes").join(",")}) OR name = 'Costumes'")
