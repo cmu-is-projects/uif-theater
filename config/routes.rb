ff13ae61dd8b85ac2a43b3cca87f959887d54a25
profh <profh@cmu.edu>
2011-07-12 22:36:58 -0400

    Browse is ajaxified

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Uifcostumes::Application.routes.draw do
   match 'addlistitem/:id' => 'items#add_list_item', :as => :add_list_item
   match 'deleteuser/:id' => 'user#destroy', :as => :destroy_user
   match 'updateuser/:id' => 'user#update', :as => :update_user
-  match 'browsesubcategories/:id' => 'items#browse_subcategories', :as => :browse_subcategories
+  match 'browsesubcategories/:id' => 'items#index', :as => :browse_subcategories
   match 'removefromlist/:id' => 'items#remove_from_list', :as => :remove_from_list
 
   # Default route
